1488d204af3222f3c48c9681862b2916c5092955
Les Pounder <lespounder@gmail.com>
2015-04-15 16:50:07 +0100

    First commit of guess.rb

---
+++ guess.rb
@@ -0,0 +1,16 @@
+# Type in your code just below here
+alert "Hello lets play a guessing game"
+number = rand(100)
+puts number
+3.times do
+  guess = ask "what is your guess?"
+  if guess > number
+  alert "Sorry your guess was too high"
+  elsif guess < number
+  alert "Sorry your guess was too low"
+  else
+  alert "Correct answer"
+  break
+  end
+alert "Congratulations you have guess the correct number"
+end
